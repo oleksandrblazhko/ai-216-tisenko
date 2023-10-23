c96e6394e623c44751d5cbf0e8a49175a01d0fe0
TisenkoVladislavAI216 <113099644+TisenkoVladislavAI216@users.noreply.github.com>
2023-10-23 13:12:33 +0300

    Update RelationDB_DetailedDataSchema.sql

--- 2-SoftwareDesign/2.4-DetailedDataSchema/RelationDB_DetailedDataSchema.sql
+++ 2-SoftwareDesign/2.4-DetailedDataSchema/RelationDB_DetailedDataSchema.sql
@@ -1,17 +1,16 @@
+
 CREATE TABLE operator
 (
-    operator_id INT PRIMARY KEY,
-    operator_id INT REFERENCES operator(operator_id),
-    FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
+    operator_id INT PRIMARY KEY
 );
 
 CREATE TABLE customer
 (
     customer_id INT PRIMARY KEY,
     email VARCHAR(30),
-    CONSTRAINT check_email_domain CHECK (email ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}$'),
-    operator_id INT REFERENCES operator(operator_id),
-    FOREIGN KEY (operator_id) REFERENCES operator(operator_id)
+    operator_id INT,
+    FOREIGN KEY (operator_id) REFERENCES operator(operator_id),
+    CONSTRAINT check_email_domain CHECK (email REGEXP '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}$')
 );
 
 CREATE TABLE humidifier 
@@ -19,7 +18,8 @@ CREATE TABLE humidifier
     humidifier_id INT PRIMARY KEY,
     name_h VARCHAR(30),
     price INT UNSIGNED,
-    customer_id INT REFERENCES customer(customer_id)
+    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
 );
 
 CREATE TABLE user
@@ -27,17 +27,19 @@ CREATE TABLE user
     user_id INT PRIMARY KEY,
     name VARCHAR(30),
     surname VARCHAR(30),
-    CONSTRAINT check_name CHECK (name NOT LIKE '%[^A-Za-z\-]%'),
-    CONSTRAINT check_surname CHECK (surname NOT LIKE '%[^A-Za-z\-]%'),
-    age INT UNSIGNED,
-    num_card INT,
-    CONSTRAINT chk_num_card_length CHECK (LENGTH(num_card::TEXT) = 16),
+    age INT,
+    num_card BIGINT, 
     valid_per DATE,
-    CONSTRAINT CheckYear CHECK (YEAR(valid_per) >= 2023),
     cvv2 INT,
-    CONSTRAINT chk_cvv2_length CHECK (LENGTH(cvv2::TEXT) = 3),
     customer_id INT UNIQUE,
     operator_id INT UNIQUE,
-    FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
+    CHECK (age > 0 AND age < 125),
+    CHECK (LENGTH(num_card) = 16),
+    CHECK (YEAR(valid_per) >= 2023),
+    CHECK (LENGTH(cvv2) = 3),
+    CHECK (name REGEXP '^[A-Za-z\\-]+$'),
+    CHECK (surname REGEXP '^[A-Za-z\\-]+$'),
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
     FOREIGN KEY (operator_id) REFERENCES operator(operator_id)
 );
+
