051d080238629b31f17e9c81b505072e9e3fb04a
TisenkoVladislavAI216 <113099644+TisenkoVladislavAI216@users.noreply.github.com>
2023-10-23 12:42:31 +0300

    Update RelationDB_DetailedDataSchema.sql

--- 2-SoftwareDesign/2.4-DetailedDataSchema/RelationDB_DetailedDataSchema.sql
+++ 2-SoftwareDesign/2.4-DetailedDataSchema/RelationDB_DetailedDataSchema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE operator
 (
-    operator_id INT PRIMARY KEY
-    operator_id INT REFERENCES operator(operator_id)
+    operator_id INT PRIMARY KEY,
+    operator_id INT REFERENCES operator(operator_id),
     FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
 );
 
@@ -9,8 +9,8 @@ CREATE TABLE customer
 (
     customer_id INT PRIMARY KEY,
     email VARCHAR(30),
-    CONSTRAINT check_email_domain CHECK (RIGHT(email, 10) = '@gmail.com' OR RIGHT(email, 8) = '@ukr.net');
-    operator_id INT REFERENCES operator(operator_id)
+    CONSTRAINT check_email_domain CHECK (Email ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}$'),
+    operator_id INT REFERENCES operator(operator_id),
     FOREIGN KEY (operator_id) REFERENCES operator(operator_id)
 );
 
@@ -27,11 +27,13 @@ CREATE TABLE user
     user_id INT PRIMARY KEY,
     name VARCHAR(30),
     surname VARCHAR(30),
+    CONSTRAINT check_name CHECK (name NOT LIKE '%[^A-Za-z\-]%'),
+    CONSTRAINT check_surname CHECK (surname NOT LIKE '%[^A-Za-z\-]%'),
     age INT UNSIGNED,
     num_card INT,
     CONSTRAINT chk_num_card_length CHECK (LENGTH(num_card::TEXT) = 16),
-    valid_per TEXT,
-    CONSTRAINT chk_valid_per_format CHECK (valid_per ~ '^(0[1-9]|1[0-2])/[0-9][0-9]$' AND RIGHT(valid_per, 2)::INT + 2000 >= 2023),
+    valid_per DATE,
+    CONSTRAINT CheckYear CHECK (YEAR(valid_per) >= 2023),
     cvv2 INT,
     CONSTRAINT chk_cvv2_length CHECK (LENGTH(cvv2::TEXT) = 3),
     customer_id INT UNIQUE,
